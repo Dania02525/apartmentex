91e330f0db601db9b2f8f1ed4c892009d97203dd
Ian C. Anderson <ian@iancanderson.com>
2016-06-29 15:55:02 -0400

    Fix compilation with newest Ecto 1.1.x
    The `put_meta/2` function was moved from Ecto.Model to top-level Ecto
    module.
    
    See https://github.com/elixir-ecto/ecto/commit/b33075098376b75008b24a115652c6c7cbd71feb

--- mix.exs
+++ mix.exs
@@ -28,6 +28,6 @@ defmodule Apartmentex.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [{:postgrex, ">= 0.0.0"},
-    {:ecto, "~> 1.0"}]
+    {:ecto, "~> 1.1.8"}]
   end
 end
