4af9bdb528cc1a93254f3e020fc6d90a2ff9f364
dania02525 <dania02525@gmail.com>
2016-07-25 17:01:07 -0500

    fix warnings in test

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -2,6 +2,8 @@ defmodule Apartmentex.TestPostgresRepo do
   use Ecto.Repo, otp_app: :apartmentex, adapter: Ecto.Adapters.Postgres, pool: Ecto.Adapters.SQL.Sandbox
 end
 
+Code.compiler_options(ignore_module_conflict: true)
+
 Mix.Task.run "ecto.drop", ["quiet", "-r", "Apartmentex.TestPostgresRepo"]
 Mix.Task.run "ecto.create", ["quiet", "-r", "Apartmentex.TestPostgresRepo"]
 
