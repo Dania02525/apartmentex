67f27361c86d1d2d7e1dd7e202a0acd820c025ff
dania02525 <dania02525@gmail.com>
2017-03-12 18:04:58 -0600

    make migrations folder configurable

--- lib/apartmentex/migrations_path_builder.ex
+++ lib/apartmentex/migrations_path_builder.ex
@@ -1,7 +1,8 @@
 defmodule Apartmentex.MigrationsPathBuilder do
   import Mix.Ecto, only: [build_repo_priv: 1]
+  @migrations_folder Application.get_env(:apartmentex, :migrations_folder) || "tenant_migrations"
 
   def tenant_migrations_path(repo) do
-    Path.join(build_repo_priv(repo), "tenant_migrations")
+    Path.join(build_repo_priv(repo), @migrations_folder)
   end
 end
