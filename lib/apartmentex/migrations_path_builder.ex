7d564c097e8a9fc5b1a71b3fcbbf2189d56bfb44
opsb <oliver@opsb.co.uk>
2017-09-13 16:17:23 +0200

    Upgrade ecto to 2.2.3

--- lib/apartmentex/migrations_path_builder.ex
+++ lib/apartmentex/migrations_path_builder.ex
@@ -1,8 +1,8 @@
 defmodule Apartmentex.MigrationsPathBuilder do
-  import Mix.Ecto, only: [build_repo_priv: 1]
+  import Mix.Ecto, only: [source_repo_priv: 1]
   @migrations_folder Application.get_env(:apartmentex, :migrations_folder) || "tenant_migrations"
 
   def tenant_migrations_path(repo) do
-    Path.join(build_repo_priv(repo), @migrations_folder)
+    Path.join(source_repo_priv(repo), @migrations_folder)
   end
 end
