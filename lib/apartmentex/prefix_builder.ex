9119edcba3507995fd74577d9f5eaa14661c0628
Robert Hetzler <robert@hetzler.ca>
2017-12-04 16:40:45 -0800

    Defer the resolution of schema_prefix until later when the Application env is available

--- lib/apartmentex/prefix_builder.ex
+++ lib/apartmentex/prefix_builder.ex
@@ -1,8 +1,11 @@
 defmodule Apartmentex.PrefixBuilder do
-  @schema_prefix Application.get_env(:apartmentex, :schema_prefix) || "tenant_"
 
-  def build_prefix(tenant) when is_integer(tenant), do: @schema_prefix <> Integer.to_string(tenant)
-  def build_prefix(tenant) when is_binary(tenant), do: @schema_prefix <> tenant
+  def schema_prefix() do
+    Application.get_env(:apartmentex, :schema_prefix) || "tenant_"
+  end
+
+  def build_prefix(tenant) when is_integer(tenant), do: schema_prefix() <> Integer.to_string(tenant)
+  def build_prefix(tenant) when is_binary(tenant), do: schema_prefix() <> tenant
   def build_prefix(tenant) do
     cond do
       is_binary(tenant.id) -> build_prefix(tenant.id)
