508a47fe39016272bb6bc0915821315bc950223e
dania02525 <dania02525@gmail.com>
2018-01-08 23:03:06 -0700

    update migration path builder to avoid mix dependency

--- lib/apartmentex/migrations_path_builder.ex
+++ lib/apartmentex/migrations_path_builder.ex
@@ -1,8 +1,15 @@
 defmodule Apartmentex.MigrationsPathBuilder do
-  import Mix.Ecto, only: [source_repo_priv: 1]
   @migrations_folder Application.get_env(:apartmentex, :migrations_folder) || "tenant_migrations"
 
   def tenant_migrations_path(repo) do
-    Path.join(source_repo_priv(repo), @migrations_folder)
+    Path.join(priv_path_for(repo), @migrations_folder)
+  end
+
+  def priv_dir(app), do: "#{:code.priv_dir(app)}"
+
+  def priv_path_for(repo) do
+    app = Keyword.get(repo.config, :otp_app)
+    repo_underscore = repo |> Module.split |> List.last |> Macro.underscore
+    Path.join([priv_dir(app), repo_underscore])
   end
 end
